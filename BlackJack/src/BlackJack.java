1655aafd45051eccb55957715ef54924a212f0ae
alex stavreski <alex_21@live.com>
2019-03-11 11:34:44 +0100

    fix : applied logic

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -5,46 +5,49 @@ public class BlackJack {
 	public static ArrayList<Person>players;
 
 	public BlackJack() {
-        startGame();
-        takeTurns();
-    }
-	
+		startGame();
+		takeTurns();
+	}
+
 	public static void main(String[] args) {
+
 		BlackJack game = new BlackJack();
 	}
 
 	public static void startGame() {
-		
-			
-			System.out.println("let's play!");
-			players = new ArrayList<Person>();
-			players.add(new Player());
-			players.add(new Dealer());
-			
+
+
+		System.out.println("let's play!");
+		players = new ArrayList<Person>();
+		players.add(new Player());
+		players.add(new Dealer());
+
 	}
-	
+
 	private static void takeTurns() {
-		
+
 		for (Person player: players) {
-            boolean endOfTurn = false;
-            while(!endOfTurn) {
-            	System.out.println("it's " + player.getName()+"'s  turn");
-                player.getCard();
-                boolean hit = player.wantToHit();
-                if (hit) {
-                    player.getCard();
-                    System.out.println(player.getName() + " drew:");
-                    if (player.getTotal() > 21) {
-                        endOfTurn = true;
-                        System.out.println(player.getName() + " lost.");
-                    }
-                } else {
-                    endOfTurn = true;
-                    System.out.println(player.getName() + " stayed.");
-                }
-            }
-        }
-		
+			boolean endOfTurn = false;
+			while(!endOfTurn) {
+				System.out.println("it's " + player.getName()+"'s  turn");
+				player.getCard();
+				boolean hit = player.wantToHit();
+				if (hit) {
+
+					System.out.println("The " + player.getName());
+					player.getCard();
+
+					if (player.getTotal() > 21) {
+						endOfTurn = true;
+						System.out.println(player.getName() + " lost.");
+					}
+				} else {
+					endOfTurn = true;
+					System.out.println(player.getName() + " stayed.");
+				}
+			}
+		}
+
 	}
 
 }
